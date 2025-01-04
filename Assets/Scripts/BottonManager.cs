adbbb12e349da09bf47b25c68ad2c29763a6a416
erpse <s0592907@htw-berlin.de>
2025-01-04 12:48:53 +0100

    Nicht fuktional!! änderungen am prefab und BottonManagerSkript

--- Assets/Scripts/BottonManager.cs
+++ Assets/Scripts/BottonManager.cs
@@ -6,40 +6,45 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 
 public class BottonManager : MonoBehaviour
-{
-    [SerializeField] private Button mainButton;
+{   
+    [SerializeField] private Button deleteButton;
+    [SerializeField] private Button regenerate_settinngs_Button;
+    [SerializeField] private Button generate_settings_Button;
+    [SerializeField] private Toggle safe_Button;
     [SerializeField] private Button regenerateButton;
-    [SerializeField] private Button newArtObjectButton;
+    [SerializeField] private Button generate_button;
     [SerializeField] private Button viewNodeButton;
+    [SerializeField] private Button exit_View_Nodes_Button;
+    [SerializeField] private Toggle connect_Worms_Button;
+    [SerializeField] private Toggle exit_Connect_Worms_Button; 
     
     [SerializeField] private Wurm artObjectScript;
     [SerializeField] private Radiuslider radiuslider;
     [SerializeField] private UnityEngine.UI.Slider slider;
+    [SerializeField] private UnityEngine.UI.Slider SmothnesSlider;
 
     [SerializeField] public InputActionAsset inputActionAsset;
     
 
-    private List<Wurm> worms = new List<Wurm>();
+    [SerializeField] private List<Wurm> worms;
 
     
 
     void Start()
     {   
-        
-        regenerateButton.gameObject.SetActive(false);
-        newArtObjectButton.gameObject.SetActive(false);
-        viewNodeButton.gameObject.SetActive(false);
-        worms.Add(artObjectScript);
+        if (worms==null)
+        {
+            worms = new List<Wurm>();
+        }
+        exit_View_Nodes_Button.gameObject.SetActive(false);
+        viewNodeButton.gameObject.SetActive(true);
     }
 
 
-   public void OnMainButtonClick()
-    {
-        mainButton.gameObject.SetActive(false);
-
-        
+    public void OnMainButtonClick()
+    {   
         regenerateButton.gameObject.SetActive(true);
-        newArtObjectButton.gameObject.SetActive(true);
+        generate_button.gameObject.SetActive(true);
         viewNodeButton.gameObject.SetActive(true);
 
         CreateWorm();
@@ -60,6 +65,16 @@ public class BottonManager : MonoBehaviour
         CreateWorm();
     }
 
+    public void OnDeleteButtonClick()
+    {
+        if (worms.Count > 0 && worms.Contains(artObjectScript))
+        {
+            int i = worms.FindIndex(x => x == artObjectScript);
+            Destroy(worms[i].gameObject);
+            worms.RemoveAt(i);
+        }
+    }
+
     private void CreateWorm()
     {
         Wurm newWurm = Instantiate(artObjectScript);
@@ -67,13 +82,16 @@ public class BottonManager : MonoBehaviour
         artObjectScript = newWurm;
 
         worms.Add(newWurm);
+        
         newWurm.OnButtonClick();
         radiuslider.setWurm(newWurm);
         InteractableUnityEventWrapper eventWrapper = newWurm.GetComponentInChildren<InteractableUnityEventWrapper>();
         eventWrapper.WhenSelect.AddListener(() => OnSelect(newWurm));
+        
         OnSelect(newWurm);
     }
     public void OnSelect(Wurm wurm){
+
         artObjectScript = wurm;
         slider.value = wurm.GetRadius();
     }
@@ -81,11 +99,12 @@ public class BottonManager : MonoBehaviour
     
     public void OnViewNodeButtonClick(bool view)
     {
-         Debug.Log("OnViewNodeButtonClick");
+        Debug.Log("OnViewNodeButtonClick");
         artObjectScript.ViewNodes(view);
+
     }
 
-    public void onValueChanged()
+    public void OnValueChanged()
     {
         if (artObjectScript == null)
         {
@@ -95,10 +114,54 @@ public class BottonManager : MonoBehaviour
 
         artObjectScript.SetRadius(slider.value);
     }
+    
+    //methode um moove/viewNode Modus zu verlassen
+    //true wenn:  view nodes aktiv ist, also die nodes sichtbar sind.
+    //false wenn: view nodes deaktiv ist, also die nodes nicht sichtbar sind.
+    public bool ExitViewNodeButtonClick()
+    {
+        if (viewNodeButton.gameObject.activeSelf == false)
+        {
+            artObjectScript.ViewNodes(false);
+            viewNodeButton.gameObject.SetActive(true);
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
+
+    public void ConnectMode(){
+        OnViewNodeButtonClick(true);
+        connect_Worms_Button.gameObject.SetActive(false);
+        exit_Connect_Worms_Button.gameObject.SetActive(true);
+    }
+    
+    public void ExitConnectMode(){
+        OnViewNodeButtonClick(false);
+        connect_Worms_Button.gameObject.SetActive(true);
+        exit_Connect_Worms_Button.gameObject.SetActive(false);
+    }
 
     public Wurm GetWurm()
     {
         return artObjectScript;
+    }
+
+    private void DeaktivateButtons()
+    {
+        regenerateButton.gameObject.SetActive(false);
+        generate_button.gameObject.SetActive(false);
+        viewNodeButton.gameObject.SetActive(false);
+        exit_View_Nodes_Button.gameObject.SetActive(false);
+        exit_Connect_Worms_Button.gameObject.SetActive(false);
+        connect_Worms_Button.gameObject.SetActive(false);
+        generate_settings_Button.gameObject.SetActive(false);
+        safe_Button.gameObject.SetActive(false);
+        slider.gameObject.SetActive(false);
+        SmothnesSlider.gameObject.SetActive(false);
+        deleteButton.gameObject.SetActive(false);
+        regenerate_settinngs_Button.gameObject.SetActive(false);
     }
 }
 
